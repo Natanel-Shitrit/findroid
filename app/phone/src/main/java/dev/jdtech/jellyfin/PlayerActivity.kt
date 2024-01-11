32f39086275f3c3c331a401d65e5a2f6cb94a3f1
Natanel Shitrit <65548905+Natanel-Shitrit@users.noreply.github.com>
2024-01-11 11:49:41 +0200

    Fix crash on episode skip

--- app/phone/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
@@ -162,8 +162,6 @@ class PlayerActivity : BasePlayerActivity() {
                                 speedButton.imageAlpha = 255
                                 pipButton.isEnabled = true
                                 pipButton.imageAlpha = 255
-
-                                loadChapters()
                             }
                         }
                     }
@@ -173,6 +171,7 @@ class PlayerActivity : BasePlayerActivity() {
                     viewModel.eventsChannelFlow.collect { event ->
                         when (event) {
                             is PlayerEvents.NavigateBack -> finish()
+                            is PlayerEvents.PlayerReady -> loadChapters()
                         }
                     }
                 }
